c70ce7aad6699b63e67b9f712972f48b76dd7c2c
Zhuotang Li <zhuotang.li@sjsu.edu>
2022-07-26 14:33:56 -0700

    random function and UI update

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -6,8 +6,18 @@ import axios from "axios";
 const MainPage = () => {
 
     let dataIdx = 0;
+    let passAllIdx = 0;
     let dataArray = [];
     let checkedImage = false;
+
+    //constant of dialogs
+    const correctAnswer = ["Correct!（￣ˇ￣）", "Keep going <（￣︶￣）>", "Good Job! ＼（￣︶￣）／"];
+    const wrongAnswer = ["Try again (*￣∇￣*)", "Almost there ╮（￣▽￣）╭", "Don't give up （｡・`ω´･）"];
+    const passAll = ["You have passed all quiz ｂ（￣▽￣）ｄ",
+        "Do you want to restart? (* ￣ー￣)",
+        "Hit the start button to restart (*￣∇￣*)",
+        "Hit the start button to restart... (*￣∇￣*)",
+        "Just hit the **** button to restart （╬￣皿￣）"];
 
     //references of html elements
     const preload_ref = useRef(null);
@@ -16,6 +26,7 @@ const MainPage = () => {
     const ans_ref = useRef(null);
     const input_ref = useRef(null);
     const sample_ref = useRef(null);
+    const quizPrompt_ref = useRef(null);
     const prompt_ref = useRef(null);
     const checkImage_ref = useRef(null);
 
@@ -63,10 +74,14 @@ const MainPage = () => {
     function startQuiz(e) {
         e.preventDefault();
         dataIdx = 0;
+        passAllIdx = 0;
+        quizPrompt_ref.current.innerHTML = "";
         //get the data from database
         axios.get(process.env.REACT_APP_API_URL)
             .then(res => {
                 dataArray = res.data;
+                //shuffle the array
+                dataArray = dataArray.sort((a1, a2) => 0.5 - Math.random());
                 if (dataArray.length === 0)
                     alert("please add more problems")
                 else
@@ -78,18 +93,20 @@ const MainPage = () => {
     //check the answer
     function sumbitAnswer(e) {
         e.preventDefault();
-        if (dataIdx >= dataArray.length)
-            alert("You have passed all quiz");
+        if (dataIdx >= dataArray.length) {
+            quizPrompt_ref.current.innerHTML = passAll[passAllIdx];
+            if (passAllIdx < 4) passAllIdx += 1;
+        }
         else if (dataArray[dataIdx].answer === input_ref.current.value.toLowerCase()) {
             dataIdx += 1;
             if (dataIdx >= dataArray.length)
-                alert("pass all quiz");
+                quizPrompt_ref.current.innerHTML = "pass all quiz!!!";
             else {
                 sample_ref.current.src = dataArray[dataIdx].img_src;
-                alert("correct");
+                quizPrompt_ref.current.innerHTML = correctAnswer[Math.floor(Math.random() * 3)];
             }
         } else {
-            alert("wrong");
+            quizPrompt_ref.current.innerHTML = wrongAnswer[Math.floor(Math.random() * 3)];
         }
     }
 
@@ -111,6 +128,7 @@ const MainPage = () => {
 
                 <div className="quiz" id="quiz">
                     <div className="quiz-container">
+                        <div className="prompt" ref={quizPrompt_ref}></div>
                         <button className="start-btn" onClick={startQuiz}> {"|> start"}</button>
                         <img className="sample" ref={sample_ref} alt="" />
                         <input className="answer" ref={input_ref} type="text" placeholder="what's the name of the animal?" />
